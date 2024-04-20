7ec9cc1fcc7d571e529e386b71737d16d8a47933
VaclavJirka <vaclav.jirka@icloud.com>
2024-04-21 00:52:46 +0200

    Updated metadata and added sitemap.xml

--- frontend/src/components/helmet_templates/StandardHelmetTemplate.js
+++ frontend/src/components/helmet_templates/StandardHelmetTemplate.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 const keywords =
-  "názvosloví, chemie, sloučeniny, názvy, názvosloví organické chemie, názvosloví anorganické chemie, názvosloví neorganické chemie, názvosloví organických sloučenin, názvosloví anorganických sloučenin, názvosloví neorganických sloučenin";
+  "názvosloví, chemické názvosloví, chemické názvosloví procvičování, procvičování názvů prvků, procvičování názvosloví sloučenin, anorganické názvosloví, názvosloví anorganických sloučenin, procvičování názvosloví, nazvoslovi, procvičování názvosloví";
 
 function StandardHelmetTemplate({ title, description }) {
   return (
@@ -9,24 +9,28 @@ function StandardHelmetTemplate({ title, description }) {
       {/* Standard metadata tags */}
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} /> {/*nahradit */}
+      <meta name="keywords" content={keywords} />
       <meta name="author" content="Václav Jirka" />
       {/* Facebook tags */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://metatags.io/" /> {/*nahradit */}
+      <meta property="og:url" content="https://nazvoslovi.eu/" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:locale" content="cs_CZ" />
-      <meta property="og:image" content={"/static/images/beaker.png"} />
-      {/*nahradit */}
+      <meta
+        property="og:image"
+        content={"/static/images/nazvoslovi_og_logo.png"}
+      />
       {/* Twitter tags */}
       <meta name="twitter:creator" content="Václav Jirka" />
-      <meta name="twitter:url" content="https://metatags.io/" /> {/*nahradit */}
+      <meta name="twitter:url" content="https://nazvoslovi.eu/" />
       <meta name="twitter:card" content="website" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta property="twitter:image" content={"/static/images/beaker.png"} />
-      {/*nahradit */}
+      <meta
+        property="twitter:image"
+        content={"/static/images/nazvoslovi_og_logo.png"}
+      />
     </Helmet>
   );
 }
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,4 +1,13 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from .views import index
+from django.views.static import serve
+from django.conf import settings
 
-urlpatterns = [re_path(r"^.*", index)]
+urlpatterns = [
+    re_path(r"^.*", index),
+    path(
+        "/sitemap.xml",
+        serve,
+        {"path": "sitemap.xml", "document_root": settings.STATIC_ROOT},
+    ),
+]
