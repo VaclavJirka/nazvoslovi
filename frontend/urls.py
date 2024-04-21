6281d7bb217f44f81f544ea1ff29873c12e9f884
VaclavJirka <vaclav.jirka@icloud.com>
2024-04-21 11:46:09 +0200

    Updated sitemap config

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,13 +1,6 @@
-from django.urls import re_path, path
+from django.urls import re_path
 from .views import index
-from django.views.static import serve
-from django.conf import settings
 
 urlpatterns = [
     re_path(r"^.*", index),
-    path(
-        "/sitemap.xml",
-        serve,
-        {"path": "sitemap.xml", "document_root": settings.STATIC_ROOT},
-    ),
 ]
